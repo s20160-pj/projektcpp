a4f7d9ced73108e45cd63ebd1485edebe824d4d9
Patryk Borucki <s20160@pjwstk.edu.pl>
2019-12-13 16:44:58 +0100

    Dodano main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include <vector>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::vector;
+
+struct gielda {
+    vector <string> nazwa;
+    vector <string> data;
+    vector <double> otwarcie;
+    vector <double> max;
+    vector <double> min;
+    vector <double> aktualny;
+    vector <double> wolumen_obrotu;
+};
+
+static size_t write(void* prt, size_t size, size_t nmemb, string* data) {
+    data->append((char*) prt, size * nmemb);
+    return size * nmemb;
+}
+
+string pobierz() {
+    string response;
+    auto curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, "https://info.bossa.pl/pub/ciagle/mstock/sesjacgl/sesjacgl.prn");
+    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    curl = NULL;
+    return response;
+}
+
+void get_gpw() {
+    gielda* gpw = new gielda;
+    char delim = ',';
+    std::istringstream input;
+    input.str(pobierz());
+    for (string line; std::getline(input, line, delim); ) {
+        gpw->nazwa.push_back(line);
+    }
+    for (int i = 0; gpw->nazwa.size() >> i; i++) {
+        cout << gpw->nazwa.at(i) << "\n";
+    }
+}
+
+
+int main() {
+    get_gpw();
+    return 0;
+}
